2ed3a7c98eb1ea1c65c1af455e41c922cafbfbc9
JPhoenix <rochavesf@hotmail.com>
2018-04-08 21:33:40 -0600

    update

--- grafos/book.ap.bridges.cpp
+++ grafos/book.ap.bridges.cpp
@@ -2,25 +2,22 @@ int dfsNumberCounter, dfsRoot, rootChildren;
 vi dfs_num, dfs_low, dfs_parent, articulation_vertex;
 
 void articulationPointAndBridge(int u) {
-  dfs_low[u] = dfs_num[u] = dfsNumberCounter++;      // dfs_low[u] <= dfs_num[u]
+  dfs_low[u] = dfs_num[u] = dfsNumberCounter++;
   for (int j = 0; j < (int)AdjList[u].size(); j++) {
     ii v = AdjList[u][j];
-    if (dfs_num[v.first] == -1) {                          // a tree edge
+    if (dfs_num[v.first] == -1) {
       dfs_parent[v.first] = u;
-      if (u == dfsRoot) rootChildren++;  // special case, count children of root
-
+      if (u == dfsRoot) rootChildren++;
       articulationPointAndBridge(v.first);
-
-      if (dfs_low[v.first] >= dfs_num[u])              // for articulation point
-        articulation_vertex[u] = true;           // store this information first
-      if (dfs_low[v.first] > dfs_num[u])                           // for bridge
+      if (dfs_low[v.first] >= dfs_num[u])
+        articulation_vertex[u] = true;
+      if (dfs_low[v.first] > dfs_num[u])
         printf(" Edge (%d, %d) is a bridge\n", u, v.first);
-      dfs_low[u] = min(dfs_low[u], dfs_low[v.first]);       // update dfs_low[u]
+      dfs_low[u] = min(dfs_low[u], dfs_low[v.first]);
     }
-    else if (v.first != dfs_parent[u])       // a back edge and not direct cycle
-      dfs_low[u] = min(dfs_low[u], dfs_num[v.first]);       // update dfs_low[u]
+    else if (v.first != dfs_parent[u])
+      dfs_low[u] = min(dfs_low[u], dfs_num[v.first]);
 } }
-
   // At main
   dfsNumberCounter = 0; dfs_num.assign(V, -1); dfs_low.assign(V, 0);
   dfs_parent.assign(V, -1); articulation_vertex.assign(V, 0);
@@ -29,7 +26,7 @@ void articulationPointAndBridge(int u) {
     if (dfs_num[i] == -1) {
       dfsRoot = i; rootChildren = 0;
       articulationPointAndBridge(i);
-      articulation_vertex[dfsRoot] = (rootChildren > 1); }       // special case
+      articulation_vertex[dfsRoot] = (rootChildren > 1); }
   printf("Articulation Points:\n");
   for (int i = 0; i < V; i++)
     if (articulation_vertex[i])
